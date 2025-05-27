8d2773a2b6cf0fbab54157d7ff8e8e059bd9e701
Benja100101 <vbenjamintr@gmail.com>
2025-05-26 21:55:56 -0500

    Eliminar especialidades no utilizadas en la clase Especialidades

--- src/entidades/Especialidades.java
+++ src/entidades/Especialidades.java
@@ -10,19 +10,14 @@ public class Especialidades {
         especialidades.put("1", "Cardiología");
         especialidades.put("2", "Cirugía General");
         especialidades.put("3", "Dermatología");
-        especialidades.put("4", "Endocrinología");
-        especialidades.put("5", "Gastroenterología");
         especialidades.put("6", "Ginecología y Obstetricia");
         especialidades.put("7", "Medicina General");
         especialidades.put("8", "Medicina Interna");
         especialidades.put("9", "Neurología");
-        especialidades.put("10", "Odontología General y Especialidades");
         especialidades.put("11", "Oftalmología");
         especialidades.put("12", "Otorrinolaringología");
         especialidades.put("13", "Pediatría");
-        especialidades.put("14", "Psicología o Psiquiatría");
         especialidades.put("15", "Traumatología y Ortopedia");
-        especialidades.put("16", "Urología");
     }
 
 public void imprimirEspecialidades() {
